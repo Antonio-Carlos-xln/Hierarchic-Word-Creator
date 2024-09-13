c08614ebf80acaf4ba7acfcb34c26774a31543c1
Antonio Carlos <antoniocarlos.s.santana1999@gmail.com>
2024-09-13 13:46:42 -0300

    Add styles, html-metadata and config for jest

---
+++ app/jest.config.ts
@@ -0,0 +1,20 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './', // Diretório da aplicação Next.js
+});
+
+// Configurações personalizadas do Jest
+const customJestConfig = {
+  preset: 'ts-jest', // Define o preset para usar ts-jest
+  setupFilesAfterEnv: ['./setupTests.ts'], // Arquivo de configuração pós-inicialização
+  moduleNameMapper: {
+    // Lidando com importações de módulos estáticos (CSS, imagens, etc.)
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom', 
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/test/?(*.)+(spec|test).ts?(x)']
+};
+
+export default createJestConfig(customJestConfig);
